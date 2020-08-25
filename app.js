2f73717a703f01a19f288749c2745d5810b9c191
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-08-25 15:31:37 -0700

    Package.json y NPM

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const {frutas, dinero} = require('./frutas');
+const cowsay = require("cowsay");
+console.log(cowsay.say({
+  text : "I am a module",
+  e : "oO",
+  T : "U "
+}));
+
 frutas.forEach(item => {
   console.count(item)
 });
