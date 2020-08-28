a0a75adc3ccc582d05bfca5e6c8a9391778eb96d
Daniel Axel Jacobo Bojorquez <46801100+DanielAxelJacoboBojorquez@users.noreply.github.com>
2020-08-27 19:58:48 -0700

    Template EJS con Express

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+//Motor de Plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req,res) => {
-  res.send('Mi respuesta desde Express')
+  res.render("index",{titulo : "titulo dinamico"})
 })
 
 app.get('/servicios', (req,res) => {
-  res.send('Estas en la pagina de servicios')
+  res.render("servicios", {tituloservicios: "titulo servicios"})
 })
 
 app.listen(port, () => {
@@ -17,5 +21,5 @@ app.listen(port, () => {
 })
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname + "/public/404.html")
+  res.status(404).render("404")
 })
